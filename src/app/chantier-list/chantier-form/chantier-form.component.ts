2b6737b19e3224bd685a5556f21f3fc2ab99347d
AHINCH ADIL <ihnichadil@gmail.com>
2020-04-24 18:51:18 +0000

    Create chantier-form.component.ts

---
+++ src/app/chantier-list/chantier-form/chantier-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chantier-form',
+  templateUrl: './chantier-form.component.html',
+  styleUrls: ['./chantier-form.component.scss']
+})
+export class ChantierFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
