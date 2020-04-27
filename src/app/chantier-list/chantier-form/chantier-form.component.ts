10405944b61cc44a1283ebeaf7d4eb3f06e95483
AHINCH ADIL <ihnichadil@gmail.com>
2020-04-27 08:12:12 +0000

    Update chantier-form.component.ts

--- src/app/chantier-list/chantier-form/chantier-form.component.ts
+++ src/app/chantier-list/chantier-form/chantier-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject, ViewChild} from '@angular/core';
+import { MAT_DIALOG_DATA,MatDialogRef} from '@angular/material/dialog';
+import { FormGroup, FormBuilder, Validators, FormControl, NgForm } from "@angular/forms";
+import { ChantierService } from './../../services/chantier.service';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs';
+import { DateAdapter} from '@angular/material/core';
+import { ChantierListComponent } from '../chantier-list.component';
 
 @Component({
   selector: 'app-chantier-form',
@@ -7,9 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChantierFormComponent implements OnInit {
 
-  constructor() { }
+
+  results$ : Observable<any[]>;
+  results_f$: Observable<any[]>;
+  startAt: BehaviorSubject<string | null> = new BehaviorSubject(''); 
+  date: any  
+  ChantierFormEdit: FormGroup  
+  @ViewChild('resetChantierForm',{static: true}) myNgForm : NgForm;  
+  constructor(      
+    public fb: FormBuilder ,
+    private chantierService : ChantierService,
+    private _adapter: DateAdapter<any>,
+    public dialogRef: MatDialogRef<ChantierListComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit() {
+    this.ChantierFormEdit= this.fb.group({
+      id : new FormControl(),
+      name: ['', Validators.required],
+      compte:[],
+      archive:[]
+    });    
+  }
+
+  /* Reactive book form */
+  onSubmit(chantier) {
+    this.chantierService.UpdateChantier(this.data.id,chantier)
+    this.dialogRef.close();   
+  }  
+
+  /* Get errors */
+  public handleError = (controlName: string, errorName: string) => {
+    return this.ChantierFormEdit.controls[controlName].hasError(errorName);
+  }   
+
+  search(searchText){
+    this.startAt.next(searchText);
+  } 
+  
+  /* Reset form */
+  closeForm() {        
+    this.dialogRef.close(); 
+    return false
   }
 
 }
