6140ccca673e4ed2ff3f98685415eed54984e6ac
AHINCH ADIL <ihnichadil@gmail.com>
2020-04-28 08:07:42 +0000

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,18 +1,57 @@
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
-import { User } from '../models/User.model';
+import { User } from '../models/engin.model';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators';
+import { firestore } from 'firebase';
 
+@Injectable({
+	providedIn: 'root'
+  })
 export class UserService {
-	private users: User[] = [
-    new User('Will', 'Alexander', 'will@will.com', 'jus d\'orange', ['coder', 'boire du café'])
-];
-	userSubject = new Subject<User[]>();
+
+	constructor(private afs: AngularFirestore){}
+
+	/* Cérer un Fournisseur */
+	AddUser(user: User){		
+		return this.afs.collection('user').doc(user.id.toString()).set({
+			createdAt: firestore.FieldValue.serverTimestamp(),       
+			firstName: user.display_name.toString(),
+			email:user.email,
+			login:user.login
+		})
+	}
 	
-	emitUsers(){
-		this.userSubject.next(this.users.slice());
+	/* Supprimer un Fournisseur */
+	async DeleteUser(id) {
+		this.afs.doc('user/'+id).delete()
+	}	
+	GetUserList(){
+		return this.afs.collection<User>('user',ref=> ref.orderBy('createdAt','asc')).snapshotChanges().pipe(
+			map(actions => {
+			return actions.map(a => {
+				const data = a.payload.doc.data() as User;
+				const id = a.payload.doc.id;
+				return { id, ...data };
+			});
+			})
+		);		
 	}
-	addUser(user: User){
-		this.users.push(user);
-		this.emitUsers();
-	}
+	//Avoir le ID du dernier enregesitrement
+	GetUserLastRecord(){		
+		return this.afs.collection('user', ref => ref
+		  .limit(1)
+		  .orderBy('createdAt','desc')
+		)				
+	  }
+	  
+	  /* Modifier un User */
+	  UpdateUser(id, user) {  		
+		  this.afs.doc('user/'+id).update({
+			display_name: user.display_name.toString(),			
+			email:user.email.toString,
+			login:user.login
+		  })														
+	  }
 
 }
