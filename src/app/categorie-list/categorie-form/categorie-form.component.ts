caed80567c3ea85aa40ee916b05b937d9af61690
AHINCH ADIL <ihnichadil@gmail.com>
2020-04-29 12:00:29 +0000

    Update categorie-form.component.ts

--- src/app/categorie-list/categorie-form/categorie-form.component.ts
+++ src/app/categorie-list/categorie-form/categorie-form.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit,Inject, ViewChild} from '@angular/core';
 import { MAT_DIALOG_DATA,MatDialogRef} from '@angular/material/dialog';
 import { FormGroup, FormBuilder, Validators, FormControl, NgForm } from "@angular/forms";
-import { CategorieService } from './../../services/categorie.service';
-import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs';
-import { DateAdapter} from '@angular/material/core';
 import { CategorieListComponent } from '../categorie-list.component';
 
 @Component({
@@ -13,17 +9,11 @@ import { CategorieListComponent } from '../categorie-list.component';
   styleUrls: ['./categorie-form.component.scss']
 })
 export class CategorieFormComponent implements OnInit {
-
-  results$ : Observable<any[]>;
-  results_f$: Observable<any[]>;
-  startAt: BehaviorSubject<string | null> = new BehaviorSubject(''); 
-  date: any  
+  
   CategorieFormEdit: FormGroup  
   @ViewChild('resetCategorieForm',{static: true}) myNgForm : NgForm;  
   constructor(      
     public fb: FormBuilder ,
-    private categorieService : CategorieService,
-    private _adapter: DateAdapter<any>,
     public dialogRef: MatDialogRef<CategorieListComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {}
 
@@ -33,26 +23,10 @@ export class CategorieFormComponent implements OnInit {
       name: ['', Validators.required],
       compte:[]
     });    
-  }
-
-  /* Reactive book form */
-  onSubmit(categorie) {
-    this.categorieService.UpdateCategorie(this.data.id,categorie)
-    this.dialogRef.close();   
-  }  
+  } 
 
   /* Get errors */
   public handleError = (controlName: string, errorName: string) => {
     return this.CategorieFormEdit.controls[controlName].hasError(errorName);
   }   
-
-  search(searchText){
-    this.startAt.next(searchText);
-  } 
-  
-  /* Reset form */
-  closeForm() {        
-    this.dialogRef.close(); 
-    return false
-  }
 }
