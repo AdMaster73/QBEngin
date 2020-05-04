2d969e9c2030088f82347efb21eb1cc681514b13
AHINCH ADIL <ihnichadil@gmail.com>
2020-05-04 15:28:14 +0000

    Update auth-guard.service.ts

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,31 +1,21 @@
-import { /*ActivatedRouteSnapshot, */CanActivate, Router/*, RouterStateSnapshot*/ } from '@angular/router';
+import { CanActivate, Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
-import * as firebase from 'firebase';
-import { logging } from 'protractor';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-  constructor(/*private authService: AuthService,*/private firebaseAuth: AngularFireAuth,
-              private router: Router) {}
+  constructor(private firebaseAuth: AngularFireAuth,
+			private router: Router) {}
 
-  canActivate(
-    /*route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot*/): Observable<boolean> | Promise<boolean> | boolean {
-    /*if(this.authService.isAuth) {
-      return true;
-    } else {
-      this.router.navigate(['/auth']);
-	}*/	
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {	
 	return new Promise(
 		(resolve,reject)=>{		
-			this.firebaseAuth.authState	.subscribe
-			/*firebase.auth().onAuthStateChanged*/(
+			this.firebaseAuth.authState	.subscribe(
 				(user)=>{
-					if(user){
-						resolve(true);
+					if(user){																		
+						resolve(true);												
 					}else{
 						this.router.navigate(['/auth','signin']);
 						resolve(false);					
