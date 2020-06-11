3964dd71b7658e82beb1202a771de24cd983ce9a
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:24:40 +0100

    Update list-nfrais-npayed.component.ts

--- src/app/list-nfrais-npayed/list-nfrais-npayed.component.ts
+++ src/app/list-nfrais-npayed/list-nfrais-npayed.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { User } from '../models/User.model';
+import { Observable, of } from 'rxjs';
+import { UserService } from '../services/user.service'
+import { tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'list-nfrais-npayed',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListNfraisNpayedComponent implements OnInit {
 
-  constructor() { }
+  listUser:Observable<User[]>
+  constructor(private userService : UserService) {
+
+    this.userService.GetUserList().pipe(
+        tap((value) => { 
+          return of(value)         
+        })
+    ).subscribe((value) => {
+        return of(value);  // 6
+    });
+
+   }
 
   ngOnInit() {
   }
 
 }
+
