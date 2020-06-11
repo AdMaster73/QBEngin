eda813eb37ec77531497543f9d483da9d82a58a9
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:19:44 +0100

    Update signup.component.ts

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 
 import Icon from '@material-ui/core/Icon';
 import Typography from '@material-ui/core/Typography';
+import { MustMatch } from './must-match.validator';
 
 @Component({
   selector: 'app-signup',
@@ -29,24 +30,40 @@ export class SignupComponent implements OnInit {
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]],
-      password_valid:['',[Validators.required,Validators.pattern(this.signupForm.controls['password'].value)]]      
-    });
+      confirmPassword:['',[Validators.required] ]     
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    });    
   }
   email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('',[Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]);
+  confirmPassword = new FormControl('',[Validators.required]);
   getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'Vous devez entrer un email valid';
+    if (this.signupForm.controls['email'].hasError('required')) {
+      return 'Champs email obligatoir';
     }
 
-    return this.email.hasError('email') ? 'email non valid' : '';
+    return this.signupForm.controls['email'].hasError('email') ? 'email non valid' : '';
   }
+  getErrorMessagePW() {
+    if (this.signupForm.controls['password'].hasError('required')) {
+      return 'Champs mot de passe obligatoir';
+    }
+    return this.signupForm.controls['password'].hasError('pattern') ? 'mot de passe doit contenir au moins 6 carachtères et des chifres' : '';
+  }
+  getErrorMessagePWC() {
+    if (this.signupForm.controls['confirmPassword'].hasError('required')) {
+      return 'Chanmps mot de passe de confirmation obligatoire';
+    }
+    return this.signupForm.controls['confirmPassword'].hasError('pattern') ? 'vous devez réentrer à nouveau votre mot de passe' : '';
+  }
+
   onSubmit() {
     const email = this.signupForm.get('email').value;
     const password = this.signupForm.get('password').value;
-    
     this.authService.createNewUser(email, password).then(
       () => {
-        this.router.navigate(['sidenav'], { replaceUrl: true });
+        this.router.navigate(['sidenav/user'], { replaceUrl: true });
       },
       (error) => {
         this.errorMessage = error;
