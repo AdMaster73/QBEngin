75c46eb98cdd0371854b723a659d5672fc911be8
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:18:54 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,20 @@ import { NewUserComponent } from './new-user/new-user.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';	
 import { AuthComponent } from './auth/auth.component';
-import {MainDashoardComponent} from './main-dashoard/main-dashoard.component';
+import { MainDashoardComponent } from './main-dashoard/main-dashoard.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { EnginListComponent } from './engin-list/engin-list.component';
 import { CategorieListComponent } from './categorie-list/categorie-list.component';
 import { FournisseurListComponent } from './fournisseur-list/fournisseur-list.component';
 import { ChantierListComponent } from './chantier-list/chantier-list.component';
-
+import { AuthAdminGuardGuard } from './services/auth-admin-guard.guard';
+import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
+import { Role } from './models/User.model'
+import { NotFoundComponent } from './not-found/not-found-component';
+import { PointageListComponent } from './pointage-list/pointage-list.component';
+import { SettingsComponent } from './settings/settings.component';
+import { TransfertComponent } from './transfert/transfert.component';
+import { EncoursAddComponent } from './transfert/encours-add/encours-add.component';
 
 const routes: Routes = [
   { path: 'sidenav', canActivate: [AuthGuardService], component: MainNavComponent,
@@ -21,22 +28,25 @@ const routes: Routes = [
           path: '',      
           pathMatch: 'full', redirectTo: 'home'
         },
-        { path: 'home', component: MainDashoardComponent },
+        { path: 'home',canActivate:[AuthGuardService], component: MainDashoardComponent },
         { path: 'engin', canActivate: [AuthGuardService], component: EnginListComponent },        
-        { path: 'users', component: UserListComponent },
-        { path: 'users/new', component: NewUserComponent },
+        { path: 'user', component: UserListComponent,canActivate:[AuthAdminGuardGuard]},  
+        { path: 'user/new', component: NewUserComponent,canActivate:[AuthAdminGuardGuard] },
         { path: 'categorie', canActivate: [AuthGuardService], component: CategorieListComponent },
         { path: 'fournisseur', canActivate: [AuthGuardService], component: FournisseurListComponent },
-        { path: 'chantier', canActivate: [AuthGuardService], component: ChantierListComponent },
-        { path: 'user', canActivate: [AuthGuardService], component: UserListComponent },
-        { path: 'signUp',canActivate:[AuthGuardService],component:SignupComponent}
+        { path: 'chantier', canActivate: [AuthGuardService], component: ChantierListComponent },        
+        { path: 'signUp',canActivate:[AuthAdminGuardGuard],component:SignupComponent},
+        { path: 'pointage',component:PointageListComponent,canActivate:[AuthAdminGuardGuard]},
+        { path: 'settings',component:SettingsComponent,canActivate:[AuthAdminGuardGuard]},
+        { path: 'transfert',component:TransfertComponent,canActivate:[AuthAdminGuardGuard]},
+        { path: 'transfert/add',component:EncoursAddComponent,canActivate:[AuthAdminGuardGuard]},
+        { path: '**', component: NotFoundComponent }
 
         ]},
-  { path: 'auth', component: AuthComponent },
   { path: '', canActivate: [AuthGuardService], pathMatch: 'full', redirectTo: 'sidenav'},
-  { path: 'new-uses', component: NewUserComponent },
-  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signUp', component: SignupComponent,canActivate:[AuthAdminGuardGuard] },
   { path: 'auth/signin', component: SigninComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
