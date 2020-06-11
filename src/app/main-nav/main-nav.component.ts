7aac26933553a89e9d0c27be1e95f6ac7a385e5a
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:24:26 +0100

    Update main-nav.component.ts

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,18 +1,28 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Observable, from, zip, pipe, Observer, combineLatest } from 'rxjs';
+import { map, shareReplay, filter, switchMap } from 'rxjs/operators';
 import { SidenavService } from './../services/sidenavService';
 import { MatSidenav } from '@angular/material';
 import { Router } from '@angular/router';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { RolesService } from '../services/roles.service';
+import { Collections, Roles } from '../models/engin.model';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss']
 })
-export class MainNavComponent  implements AfterViewInit{
-  
+export class MainNavComponent  implements AfterViewInit, OnInit{
+
+  user$: Observable<{}>;
+  collections$ : Observable<Collections[]>;
+  roleUser: string[]=[]; 
+  roleCurrentUser:string; 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
@@ -22,12 +32,38 @@ export class MainNavComponent  implements AfterViewInit{
     shouldShow = true;
     toggle() { this.shouldShow = !this.shouldShow; }       
   @ViewChild('sidenavNotification',{'static':false}) public sidenav: MatSidenav 
-  constructor(private breakpointObserver: BreakpointObserver,
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private afs: AngularFirestore,
+    private rolesService:RolesService,
+    private firebaseAuth: AngularFireAuth,    
     private sidenavService: SidenavService,
-    public router: Router) {}
+    private authService: AuthService,
+    public router: Router) {       
+      (async () => {
+          this.roleCurrentUser = await (await firebase.auth().currentUser.getIdTokenResult()).claims.role
+          this.collections$ = new Observable((observer: Observer<Collections[]>) => {
+          this.rolesService.getRolesByNameAndType(this.roleCurrentUser).subscribe(roles=>{
+            roles.forEach(item=>{
+              this.afs
+              .collection<Collections>('collections',ref=>ref.where(firebase.firestore.FieldPath.documentId(),'in',item.list)).valueChanges()
+              .subscribe(result=>{              
+                observer.next(result)
+              })                     
+            })
+          }) 
+        })                             
+      })();         
+    }   
+  ngOnInit(){                
+    this.user$ = this.firebaseAuth.user.pipe(
+      filter(user => !!user),
+      switchMap(user => this.authService.user$(user.uid))
+    )                              
+		        
+  }
     ngAfterViewInit(): void {
       this.sidenavService.setSidenav(this.sidenav);      
-      //this.router.navigate(['/sidenav']);
     }    
-    
+
 }
