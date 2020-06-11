ef669881fba368345bb63927f774e374f56afda1
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:24:31 +0100

    Update engin.model.ts

--- src/app/models/engin.model.ts
+++ src/app/models/engin.model.ts
@@ -27,10 +27,56 @@ export interface Engin {
     compte:string,
     archive:number,
   }
-  export interface User{
-    id:number,
-    display_name:string,
-    email:string,
-    login:string,    
+  export interface chantierUser{
+    createdBy: string
+    displayName: string
+    email: string
+    role: string
+    uid: string
   }
-
+  export interface User{
+    uid:string,
+    email:string,
+    displayName: string,
+    role?:string   
+  }
+  export interface Roles{
+    id:number,
+    name:string,  
+    intitule:string,
+    list?:string[],
+    add?:string[],
+    update?:string[],
+    delete?:string[]
+  }
+  export interface Collections{
+    id:number,
+    name:string,
+    intitule?:string,
+    toolTipe?:string,
+    icon?:string
+  }
+  export interface Permissions{
+    id:number,
+    collection:Collections,
+    /* list:Roles,
+    add:Roles,
+    update:Roles,
+    delete:Roles */
+  }
+  export interface Transfert{
+    id:number,
+    uid:string,
+    date_t:Date,
+    chantier_p:Chantier,
+    chantier_d:Chantier,
+    porte_char:Engin,
+    niveau_gasoil:string,
+    etat_engin:Etat_engin_Transfert
+  }
+  export enum Etat_engin_Transfert{
+    BIEN,MAUVAIS,FERAI
+  }
+  export enum Etat_engin{
+    MARCHE,ARRET,MAD,ANATTENTE,PANNE
+  }
