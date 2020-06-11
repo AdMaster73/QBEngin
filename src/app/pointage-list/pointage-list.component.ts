6f54d982b74d8f4129d59e7655975677df89076e
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:27:48 +0100

    Create pointage-list.component.ts

---
+++ src/app/pointage-list/pointage-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pointage-list',
+  templateUrl: './pointage-list.component.html',
+  styleUrls: ['./pointage-list.component.scss']
+})
+export class PointageListComponent implements OnInit {
+ 
+  constructor(){           
+  }  
+  ngOnInit(){
+    
+  }
+}
