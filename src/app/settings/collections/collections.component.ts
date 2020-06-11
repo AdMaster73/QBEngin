6ac77b602aaeb6c643722258e06cdc21061db4b5
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:28:31 +0100

    Create collections.component.ts

---
+++ src/app/settings/collections/collections.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort, MatDialog, MatDialogConfig} from '@angular/material';
+import { MatPaginator} from '@angular/material/paginator';
+import { CollectionsService } from '../../services/collections.service';
+import { Collections } from './../../models/engin.model';
+
+import { AngularFirestore } from 'angularfire2/firestore';
+import { CollectionsFormComponent } from './collections-form/collections-form.component';
+import { CollectionsAddComponent } from './collections-add/collections-add.component';
+
+@Component({
+  selector: 'app-collections',
+  templateUrl: './collections.component.html',
+  styleUrls: ['./collections.component.scss']
+})
+export class CollectionsComponent implements OnInit {
+
+  constructor(
+    public db : AngularFirestore,
+    private rollectionsServices : CollectionsService, 
+    public dialog: MatDialog) {}
+  displayedColumns: string[] = ['action','numero', 'designation','intitule','toolTipe','icon'];
+  dataSource : MatTableDataSource<Collections>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static : true}) sort: MatSort;  
+  ngOnInit(): void {
+    this.rollectionsServices.GetCollectionsList().subscribe(
+      data => {
+        this.dataSource = new MatTableDataSource(data);  
+        this.dataSource.paginator = this.paginator;        
+        this.dataSource.sort = this.sort;
+        this.paginator._intl.itemsPerPageLabel = 'Affichage par page.';
+        this.paginator._intl.firstPageLabel = 'Page Premier';
+        this.paginator._intl.nextPageLabel = 'Page Suivant';
+        this.paginator._intl.previousPageLabel = 'Page Précédante';
+        this.paginator._intl.lastPageLabel = 'Dérnier Page';
+      }
+    )
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+      this.dataSource.sort = this.sort;
+    }
+  }
+  /** Ajouter nouveau Collections */
+  addCollections(): void{
+    const dialogRef = this.dialog.open(CollectionsAddComponent);
+  }
+  /**Modifier Collections */
+  editCollections(index:number, element){   
+    const dialogConfig = new MatDialogConfig();            
+    this.dialog.open(CollectionsFormComponent,{data:{
+      id:element.id,
+      name:element.name,
+      intitule:element.intitule,
+      toolTipe:element.toolTipe,
+      icon:element.icon
+    }}).afterClosed().subscribe(result => {
+      if (result){
+        this.rollectionsServices.UpdateCollections(result)
+      } 
+    });                      
+  }
+  /* Delete */
+  deleteCollections(index: number){    
+    if(window.confirm('Are you sure?')) {
+      const data = this.dataSource.data;
+      data.splice((this.paginator.pageIndex * this.paginator.pageSize) + index, 1);
+      this.dataSource.data = data;
+      this.rollectionsServices.DeleteCollections(index)
+    }
+  }
+
+}
