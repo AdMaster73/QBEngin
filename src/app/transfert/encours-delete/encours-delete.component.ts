041ba25dd2837034dd5b1d62a8cc0728a7024ea1
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:30:57 +0100

    Create encours-delete.component.ts

---
+++ src/app/transfert/encours-delete/encours-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-encours-delete',
+  templateUrl: './encours-delete.component.html',
+  styleUrls: ['./encours-delete.component.scss']
+})
+export class EncoursDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
