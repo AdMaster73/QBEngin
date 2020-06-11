c51a04b0f32f9e31eb0960d431f9c6f07a9b12f1
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:29:32 +0100

    Create collections-delete.component.ts

---
+++ src/app/settings/collections/collections-delete/collections-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collections-delete',
+  templateUrl: './collections-delete.component.html',
+  styleUrls: ['./collections-delete.component.scss']
+})
+export class CollectionsDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
