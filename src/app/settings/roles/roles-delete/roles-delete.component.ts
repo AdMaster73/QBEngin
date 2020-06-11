18db193a2d42d2bf496955f94f472967bb7fc1b7
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:30:10 +0100

    Create roles-delete.component.ts

---
+++ src/app/settings/roles/roles-delete/roles-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-delete',
+  templateUrl: './roles-delete.component.html',
+  styleUrls: ['./roles-delete.component.scss']
+})
+export class RolesDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
