48f9595e74ad8f7186b7c45a6908d35cfcf4bc5c
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:28:44 +0100

    Create permissions-delete.component.ts

---
+++ src/app/settings/permissions/permissions-delete/permissions-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-permissions-delete',
+  templateUrl: './permissions-delete.component.html',
+  styleUrls: ['./permissions-delete.component.scss']
+})
+export class PermissionsDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
