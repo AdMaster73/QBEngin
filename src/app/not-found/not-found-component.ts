c4e14b6394f31bedd23313fcbeadf87b9c8fecf2
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:24:52 +0100

    Create not-found-component.ts

---
+++ src/app/not-found/not-found-component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found-component.html',
+  styleUrls: ['./not-found-component.scss']
+})
+export class NotFoundComponent implements OnInit {
+
+  path: string;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+  }
+
+}
