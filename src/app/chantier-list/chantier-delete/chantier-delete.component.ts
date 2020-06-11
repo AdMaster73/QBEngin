3cf8e6f775f21e99d37485331248f646930adb2b
AHINCH ADIL <ihnichadil@gmail.com>
2020-06-11 22:20:52 +0100

    Create chantier-delete.component.ts

---
+++ src/app/chantier-list/chantier-delete/chantier-delete.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-chantier-delete',
+  templateUrl: './chantier-delete.component.html',
+  styleUrls: ['./chantier-delete.component.scss']
+})
+export class ChantierDeleteComponent implements OnInit {
+
+  form = new FormGroup({
+    id: new FormControl('')
+  });
+  constructor(@Inject(MAT_DIALOG_DATA) public data:number) { }
+
+  ngOnInit() {
+  }
+
+}
