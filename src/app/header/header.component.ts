ee21d05908226ac26e695f02cd8dec2f3beb1f52
AHINCH ADIL <ihnichadil@gmail.com>
2020-07-09 15:25:36 +0100

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,12 +3,13 @@ import { AuthService } from '../services/auth.service';
 import { MatBottomSheet, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { SidenavService } from './../services/sidenavService';
 import { Router } from '@angular/router';
-import { Observable, Observer } from 'rxjs';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { UserService } from '../services/user.service'
 import { filter, switchMap } from 'rxjs/operators';
-import { Roles } from '../models/engin.model';
+import { Roles, Notification, Engin } from '../models/engin.model';
+import { async } from '@angular/core/testing';
+import { EnginService } from '../services/engin.service';
 
 @Component({
   selector: 'app-header',
@@ -16,37 +17,52 @@ import { Roles } from '../models/engin.model';
   styleUrls: ['./header.component.scss'],
   outputs:['navToggle']
 })
-export class HeaderComponent implements OnInit {  
+export class HeaderComponent implements OnInit {
 
   title = "GCR Materiel";
   emailVerified : boolean;
   user$: Observable<{}>;
   roles$: Observable<Roles>
+  listNotification : Observable <{}>
+  countNot : number
 
   customStyle = {
     backgroundColor: '#F4F4F4',
-    //border: '1px solid #bdc3c7',
     borderRadius: '50%',
     color: 'white',
     cursor: 'pointer'
   };
-  
-  constructor(public authService: AuthService,private router : Router,
-    private _bottomSheet: MatBottomSheet,private afs: AngularFirestore,public userService: UserService,
-    private sidenavService: SidenavService,private firebaseAuth: AngularFireAuth) { 
+
+  constructor(
+    public authService: AuthService,
+    private router : Router,
+    private _bottomSheet: MatBottomSheet,
+    public userService: UserService,
+    private sidenavService: SidenavService,
+    private firebaseAuth: AngularFireAuth) {
     }
 
   toggleRightSidenav() {
       this.sidenavService.toggle();
-  }  
+  }
   openBottomSheet(): void {
+
+    if(this.countNot == 0){
+      return
+    }
     this._bottomSheet.open(BottomSheetOverviewExampleSheet);
   }
-  ngOnInit() {                 
+  ngOnInit() {
     this.user$ = this.firebaseAuth.user.pipe(
       filter(user => !!user),
       switchMap(user => this.authService.user$(user.uid))
-    )    
+    )
+
+    this.listNotification = this.sidenavService.getNotificationByUser()
+
+    this.sidenavService.getNotificationByUser().subscribe(results=>{
+      this.countNot = results.length
+    })
   }
 
   onSignOut() {
@@ -66,23 +82,32 @@ export class HeaderComponent implements OnInit {
 @Component({
   selector: 'bottom-sheet-overview-avatar-sheet',
   templateUrl: 'bottom-sheet-overview-avatar-sheet.html',
+  styles:[`:host /deep/ .mat-list-item:hover .mat-icon-button {
+                display: block;
+              }
+              :host /deep/ .mat-icon-button {
+                display: none;
+              }
+              :host /deep/ .mat-line {
+                font-size: smaller !important;
+              }
+`]
 })
 export class BottomSheetOverviewExampleSheet {
-  photoUrl = "";
-  name ="";
-  email = "";
-  uid = "";
-  emailVerified:boolean;  
+  listNotification : Observable <Notification[]>
+  engins: Observable<Engin[]>
   constructor(private _bottomSheetRef: MatBottomSheetRef<BottomSheetOverviewExampleSheet>
-    ,private firebaseAuth: AngularFireAuth) {}
-  ngOnInit() {        
-    var user = this.firebaseAuth.auth.currentUser
-    this.name = user.displayName;
-    this.email = user.email;
-    this.photoUrl = user.photoURL;
-    this.emailVerified = user.emailVerified;
-    this.uid = user.uid;
+    ,private sidenaveService: SidenavService,private enginService:EnginService) {
+    }
+  ngOnInit() {
+    this.listNotification = this.sidenaveService.getNotificationByUser()
+
   }
+
+  getEnginByID(engin){
+    return this.enginService.getEnginById(engin)
+  }
+
   openLink(event: MouseEvent): void {
     this._bottomSheetRef.dismiss();
     event.preventDefault();
