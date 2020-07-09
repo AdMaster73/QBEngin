e02f2be8a712647516bcc3c663478b0d2077abd4
AHINCH ADIL <ihnichadil@gmail.com>
2020-07-09 15:25:50 +0100

    Update sidenavService.ts

--- src/app/services/sidenavService.ts
+++ src/app/services/sidenavService.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { MatSidenav } from '@angular/material';
 import { Router } from '@angular/router';
+import * as firebase from 'firebase';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators';
+import { Notification } from '../models/engin.model'
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +12,22 @@ import { Router } from '@angular/router';
 export class SidenavService {
     private sidenav: MatSidenav;
 
+    constructor(public router: Router,private afs: AngularFirestore) { }
+
+    getNotificationByUser(){
+      return this.afs.collection<Notification>('notification',
+      ref=>ref.where('uid','==',firebase.auth().currentUser.uid)
+      ).snapshotChanges().pipe(
+        map(action=>{
+          return action.map(a=>{
+            const data = a.payload.doc.data() as Notification;
+            const id = a.payload.doc.id;
+            const lenght = a.payload.doc.data.length
+            return { id,lenght, ...data };
+          })
+        })
+      )
+    }
 
     public setSidenav(sidenav: MatSidenav) {
         this.sidenav = sidenav;
@@ -23,8 +43,8 @@ export class SidenavService {
     }
 
     public toggle(): void {
-    this.sidenav.toggle();
+      this.sidenav.toggle();
    }
-   
-  constructor(public router: Router) { }
+
+
 }
