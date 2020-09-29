c09222d2cc7023771c2f528eb1f90a60c1b3191d
AHINCH ADIL <ihnichadil@gmail.com>
2020-09-29 14:35:29 +0100

    Update user-delete.component.ts

--- src/app/user-list/user-delete/user-delete.component.ts
+++ src/app/user-list/user-delete/user-delete.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { User } from 'firebase';
+import { User } from '../../models/engin.model';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
