a8e32a06959bb74261300876df12e5521e875f61
AHINCH ADIL <ihnichadil@gmail.com>
2020-09-29 09:01:55 +0100

    Update chantier.service.ts

--- src/app/services/chantier.service.ts
+++ src/app/services/chantier.service.ts
@@ -11,15 +11,29 @@ import { AngularFireAuth } from 'angularfire2/auth';
 export class ChantierService {
 
 	constructor(private afs: AngularFirestore,private firebaseAuth: AngularFireAuth) {}
-		 
+
+  /**Récuperer le chantier par utilisateur */
+
+  getChantierByUser(){
+    return this.afs.collection('chantier',ref=>ref.where('users','array-contains',this.firebaseAuth.auth.currentUser.uid)).snapshotChanges().pipe(
+      map(action =>{
+        return action.map(a =>{
+          const data = a.payload.doc.data() as Chantier;
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        })
+      })
+    )
+  }
+
 	/* Créer un nouveau chantier */
-	AddChantier(chantier: Chantier){		
-		return this.afs.collection('chantier').doc(chantier.id.toString()).set({      
+	AddChantier(chantier: Chantier){
+		return this.afs.collection('chantier').doc(chantier.id.toString()).set({
 			createdBy: this.firebaseAuth.auth.currentUser.uid,
-			createdAt: firestore.FieldValue.serverTimestamp(),	    
+			createdAt: firestore.FieldValue.serverTimestamp(),
 			name: chantier.name,
 			compte:chantier.compte,
-			archive:chantier.archive,		
+			archive:chantier.archive,
 		})
 	}
 
@@ -27,9 +41,9 @@ export class ChantierService {
 	async DeleteChantier(id) {
 		this.afs.doc('chantier/'+id).delete()
 	}
-	
+
 	/*Retourner une liste des chantier */
-	GetChantierList() {		
+	GetChantierList() {
 		return this.afs.collection<Chantier>('chantier',ref=> ref.orderBy('createdAt','asc')).snapshotChanges().pipe(
 			map(actions => {
 			return actions.map(a => {
@@ -38,33 +52,34 @@ export class ChantierService {
 				return { id, ...data };
 			});
 			})
-		);	
+		);
 	}
-	
+
 	//Avoir le ID du dernier enregesitrement
-	GetChantierLastRecord(){		
+	GetChantierLastRecord(){
 	  return this.afs.collection('chantier', ref => ref
 		.limit(1)
 		.orderBy('createdAt','desc')
-	  )				
+	  )
 	}
-	
+
 	/* Modifier un chantier */
-	UpdateChantier(chantier) {  
-		let archive:number
+	UpdateChantier(chantier) {
+    let archive:number
 		if(typeof(chantier.archive)==='number'){
 			archive = chantier.archive
 		}else{
-			!!chantier.archive ? archive = 1 : archive = 0			
+			!!chantier.archive ? archive = 1 : archive = 0
 		}
 		this.afs.doc('chantier/'+chantier.id).update(
 			{
 			uodatedBy: this.firebaseAuth.auth.currentUser.uid,
-			updatedAt: firestore.FieldValue.serverTimestamp(), 
+			updatedAt: firestore.FieldValue.serverTimestamp(),
 			name: chantier.name.toUpperCase(),
 			compte:chantier.compte,
-			archive:archive,				
+			archive:archive,
+			region:chantier.region
 			}
-		)														
-	}  
+		)
+	}
 }
