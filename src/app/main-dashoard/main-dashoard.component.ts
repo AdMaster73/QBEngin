da56da711b48d7ce60250dfa16c1492e0f734529
AHINCH ADIL <ihnichadil@gmail.com>
2020-09-29 09:01:13 +0100

    Update main-dashoard.component.ts

--- src/app/main-dashoard/main-dashoard.component.ts
+++ src/app/main-dashoard/main-dashoard.component.ts
@@ -1,39 +1,22 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Chantier } from '../models/engin.model';
+import { ChantierService } from '../services/chantier.service';
+
 
 @Component({
   selector: 'app-main-dashoard',
   templateUrl: './main-dashoard.component.html',
   styleUrls: ['./main-dashoard.component.scss']
 })
-export class MainDashoardComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      /* if (matches) {
-        return [
-          { title: 'DP',libel:'34247.18',purcent:10,trending:'up',color:'primary', cols: 1, rows: 1,notification:true,numberCols:1,date:'01/12/2020' },
-          { title: 'DS',libel:'26500',purcent:30,trending:'up',color:'accent', cols: 1, rows: 1,notification:true,numberCols:2,date:'01/12/2020' },
-          { title: 'DPS',libel:'39496.36',purcent:20,trending:'down',color:'warn', cols: 1, rows: 1,notification:true,numberCols:3,date:'01/12/2020' },
-          { title: 'DM',libel:'40128.69',purcent:40,trending:'up',color:'primary', cols: 1, rows: 1,notification:true,numberCols:4,date:'01/12/2020' },
-          { title: 'DS',libel:'1250.25',purcent:40,trending:'',color:'', cols: 2, rows: 2,notification:false,numberCols:1,date:'01/12/2020' },
-          { title: '',libel:'',purcent:40,trending:'',color:'', cols: 2, rows: 2,notification:false,numberCols:2,date:'01/12/2020' },
-          { title: '',libel:'',purcent:40,trending:'up',color:'primary', cols: 4, rows: 3,notification:false,numberCols:1,date:'01/12/2020' },          
-        ];
-      } */
+export class MainDashoardComponent implements OnInit{
 
-      return [
-          { title: 'DP',libel:'34247.18',purcent:10,trending:'up',color:'primary', cols: 1, rows: 1,notification:true,numberCols:1,date:'01/12/2020' },
-          { title: 'DS',libel:'26500',purcent:30,trending:'up',color:'accent', cols: 1, rows: 1,notification:true,numberCols:2,date:'01/12/2020' },
-          { title: 'DPS',libel:'39496.36',purcent:20,trending:'down',color:'warn', cols: 1, rows: 1,notification:true,numberCols:3,date:'01/12/2020' },
-          { title: 'DM',libel:'40128.69',purcent:40,trending:'up',color:'primary', cols: 1, rows: 1,notification:true,numberCols:4,date:'01/12/2020' },
-          { title: 'DS',libel:'1250.25',purcent:0,trending:'',color:'', cols: 2, rows: 2,notification:false,numberCols:1,date:'01/12/2020' },
-          { title: '',libel:'',purcent:40,trending:'',color:'', cols: 2, rows: 2,notification:false,numberCols:2,date:'01/12/2020' },
-          { title: '',libel:'',purcent:0,trending:'up',color:'primary', cols: 4, rows: 3,notification:false,numberCols:1,date:'01/12/2020' },          
-      ];
-    })
-  );
+  chantier$ : Observable<Chantier[]>;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private chantierService : ChantierService
+    ) {}
+  ngOnInit() {
+    this.chantier$ = this.chantierService.getChantierByUser()
+  }
 }
