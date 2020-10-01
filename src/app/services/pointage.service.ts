0b6cbea3febec849222b635142d856102b2267d0
AHINCH ADIL <ihnichadil@gmail.com>
2020-10-01 12:11:50 +0100

    Update pointage.service.ts

--- src/app/services/pointage.service.ts
+++ src/app/services/pointage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { map } from 'rxjs/operators';
-import { Chantier } from '../models/engin.model';
+import { Chantier, Engin, Pointage } from '../models/engin.model';
 declare var require: any;
 const firebase = require('firebase')
 
@@ -11,15 +11,24 @@ const firebase = require('firebase')
   providedIn: 'root'
 })
 export class PointageService {
-    
+
   constructor(private afs: AngularFirestore,
-		private firebaseAuth: AngularFireAuth) {            
+		private firebaseAuth: AngularFireAuth) {
     }
-    
-    getChantierByUser(){    
-      return this.afs.collection<Chantier>('chantier',ref=>{        
+    getPointageByEngin(engin: Engin) {
+      return this.afs.collection<Pointage>('engin/'+engin.id+'/pointage').snapshotChanges().pipe(map(action=>{
+        return action.map(a=>{
+          const id = a.payload.doc.id
+          const data = a.payload.doc.data() as Pointage
+          return {id, ...data}
+        })
+      }))
+    }
+
+    getChantierByUser(){
+      return this.afs.collection<Chantier>('chantier',ref=>{
         const id = this.firebaseAuth.auth.currentUser.uid
-        let query : firebase.firestore.Query = ref      
+        let query : firebase.firestore.Query = ref
         query = query.where('users','array-contains',id)
         return query
       }).snapshotChanges().pipe(map(action=>{
@@ -28,11 +37,11 @@ export class PointageService {
           const data = a.payload.doc.data() as Chantier
           return {id, ...data}
         })
-      }))            
+      }))
     }
     getAllChantierActive(){
-      return this.afs.collection<Chantier>('chantier',ref=>{                
-        let query : firebase.firestore.Query = ref      
+      return this.afs.collection<Chantier>('chantier',ref=>{
+        let query : firebase.firestore.Query = ref
         query = query.where('archive','==',0)
         return query
       }).snapshotChanges().pipe(map(action=>{
