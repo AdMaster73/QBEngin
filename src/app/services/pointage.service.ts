b6fb28cfa894d5b8d1c28ff6d6a384430e070f49
AHINCH ADIL <ihnichadil@gmail.com>
2020-10-01 20:46:43 +0100

    Update pointage.service.ts

--- src/app/services/pointage.service.ts
+++ src/app/services/pointage.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { map } from 'rxjs/operators';
-import { Chantier, Engin, Pointage } from '../models/engin.model';
+import { flatMap, map, mergeMap, switchMap } from 'rxjs/operators';
+import { Chantier, Engin, Pointage, User } from '../models/engin.model';
+import { AuthService} from '../services/auth.service';
+import { Observable, combineLatest, of } from 'rxjs';
 declare var require: any;
 const firebase = require('firebase')
 
@@ -12,17 +14,23 @@ const firebase = require('firebase')
 })
 export class PointageService {
 
-  constructor(private afs: AngularFirestore,
+  constructor(private afs: AngularFirestore,private authService:AuthService,
 		private firebaseAuth: AngularFireAuth) {
     }
-    getPointageByEngin(engin: Engin) {
-      return this.afs.collection<Pointage>('engin/'+engin.id+'/pointage').snapshotChanges().pipe(map(action=>{
-        return action.map(a=>{
-          const id = a.payload.doc.id
-          const data = a.payload.doc.data() as Pointage
-          return {id, ...data}
-        })
-      }))
+     getPointageByEngin(engin: Engin) {
+
+      return this.afs.collection<Pointage>('engin/'+engin.id+'/pointage').snapshotChanges().pipe(
+        switchMap((pointages)=>{
+          const point = pointages.map(p=>{
+            return this.authService.user$(p.payload.doc.data().uid)
+            .pipe(
+              map(users=>Object.assign(p,{users}))
+            )
+          })
+          return combineLatest(...point)
+        }
+        )
+      )
     }
 
     getChantierByUser(){
