b674e67546ccca8b6c229848c31825673c07ac37
AHINCH ADIL <ihnichadil@gmail.com>
2020-10-08 15:43:44 +0100

    Create chauffeur-delete.component.ts

---
+++ src/app/chauffeur-list/chauffeur-delete/chauffeur-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chauffeur-delete',
+  templateUrl: './chauffeur-delete.component.html',
+  styleUrls: ['./chauffeur-delete.component.scss']
+})
+export class ChauffeurDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
