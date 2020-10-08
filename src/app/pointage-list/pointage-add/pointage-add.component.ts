504ba28a90e2206496e04479a2f3cf16cdf974ea
AHINCH ADIL <ihnichadil@gmail.com>
2020-10-08 09:39:22 +0100

    Update pointage-add.component.ts

--- src/app/pointage-list/pointage-add/pointage-add.component.ts
+++ src/app/pointage-list/pointage-add/pointage-add.component.ts
@@ -7,6 +7,8 @@ import { Chantier } from 'src/app/models/engin.model';
 import { ChantierService } from 'src/app/services/chantier.service';
 import { PointageListComponent } from '../pointage-list.component';
 import { map, startWith } from 'rxjs/operators';
+import {DragDropModule, transferArrayItem} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-pointage-add',
@@ -18,7 +20,7 @@ import { map, startWith } from 'rxjs/operators';
 })
 export class PointageAddComponent implements OnInit {
 
-  PointageFormAddFirst: FormGroup
+  PointageFormAdd: FormGroup
   PointageFormAddSecond: FormGroup
   PointageFormAddThird: FormGroup
   options: string[] = [];
@@ -41,17 +43,17 @@ export class PointageAddComponent implements OnInit {
 
   ngOnInit() {
     this._adapter.setLocale('fr');
-    this.engin_etat = this.data.engin.etat_f
-    this.compteur_etat = this.data.engin.etat_k
-    if(this.data.engin.etat_f == 'MARCHE'){
+    this.engin_etat = this.data.etat_f
+    this.compteur_etat = this.data.etat_k
+    if(this.data.etat_f == 'MARCHE'){
       this.heure_m=10
       this.heure_p=0
       this.heure_ar=0
-    }else if(this.data.engin.etat_f == 'ARRET'){
+    }else if(this.data.etat_f == 'ARRET'){
       this.heure_m=0
       this.heure_p=0
       this.heure_ar=10
-    }else if(this.data.engin.etat_f == 'PANNE'){
+    }else if(this.data.etat_f == 'PANNE'){
       this.heure_m=0
       this.heure_p=10
       this.heure_ar=0
@@ -61,8 +63,8 @@ export class PointageAddComponent implements OnInit {
       this.heure_ar=0
     }
     this.minDate = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
-    if(this.data.chantier){
-      this.chantierService.getChantierById(this.data.chantier).subscribe(chantiers=>{
+    if(this.data.id_chantier){
+      this.chantierService.getChantierById(this.data.id_chantier).subscribe(chantiers=>{
         chantiers.forEach(chantier=>{
           this.chantier_name = chantier.name
         })
@@ -75,100 +77,123 @@ export class PointageAddComponent implements OnInit {
       })
     })
 
-    this.PointageFormAddFirst= this.fb.group({
-      date_pointage:new FormControl({value:new Date(),disabled: true}, Validators.required),
+    this.PointageFormAdd= this.fb.group({
+      date_pointage:new FormControl(Validators.required),
       etat_f: new FormControl(),
+      vidange: new FormControl(),
+      id_chantier: new FormControl(),
       heure_m: new FormControl(),
       heure_ar: new FormControl(),
       heure_p: new FormControl(),
-    });
-
-    this.PointageFormAddSecond= this.fb.group({
       gasoil: new FormControl(),
-      bon: [],
-      etat_compt: new FormControl({value:this.data.engin.etat_k}, Validators.required),
+      bon: new FormControl(),
+      etat_compt: new FormControl({value:this.data.etat_k}, Validators.required),
       compteur_anc: new FormControl({ value: '', disabled: true }),
-      compteur_nvx: new FormControl(this.data.engin.compteur, [Validators.required,Validators.min(this.data.engin.compteur)]),
-      consomation: new FormControl({ value: '', disabled: true }),
+      compteur_nvx: new FormControl(this.data.compteur, [Validators.min(this.data.compteur)]),
+      consomation: new FormControl(),
+      oil_10: new FormControl(),
+      oil_40: new FormControl(),
+      oil_90: new FormControl()
     });
-
-    this.PointageFormAddThird= this.fb.group({
-      h10: [],
-      h40: [],
-      h90: [],
-    });
-
-    this.PointageFormAddSecond.get('gasoil').valueChanges.subscribe(value => {
-      var consomation = (value*100)/(this.PointageFormAddSecond.get('compteur_nvx').value-this.PointageFormAddSecond.get('compteur_anc').value)
+    this.PointageFormAdd.get('oil_10').setValue(0)
+    this.PointageFormAdd.get('oil_40').setValue(0)
+    this.PointageFormAdd.get('oil_90').setValue(0)
+    this.PointageFormAdd.get('gasoil').setValue(0)
+    this.PointageFormAdd.get('consomation').setValue(0)
+    this.PointageFormAdd.get('bon').setValue('')
+    this.PointageFormAdd.get('date_pointage').setValue(new Date())
+    this.PointageFormAdd.get('gasoil').valueChanges.subscribe(value => {
+      var consomation = (value*100)/(this.PointageFormAdd.get('compteur_nvx').value-this.PointageFormAdd.get('compteur_anc').value)
       if(consomation == Infinity){
         return
-      }else if(consomation == NaN){
-        this.PointageFormAddSecond.get('consomation').setValue(0)
+      }else if(this.PointageFormAdd.get('compteur_nvx').value-this.PointageFormAdd.get('compteur_anc').value == 0){
+        this.PointageFormAdd.get('consomation').setValue(0)
         return
       }else if(consomation < 0){
-        this.PointageFormAddSecond.get('consomation').setValue(0)
+        this.PointageFormAdd.get('consomation').setValue(0)
       }else{
-        this.PointageFormAddSecond.get('consomation').setValue(Math.round(consomation))
+        this.PointageFormAdd.get('consomation').setValue(Math.round(consomation))
       }
     })
 
-    this.PointageFormAddSecond.get('compteur_nvx').valueChanges.subscribe(value => {
-      var consomation = (this.PointageFormAddSecond.get('gasoil').value*100)/(value-this.PointageFormAddSecond.get('compteur_anc').value)
+    this.PointageFormAdd.get('compteur_nvx').valueChanges.subscribe(value => {
+      var consomation = (this.PointageFormAdd.get('gasoil').value*100)/(value-this.PointageFormAdd.get('compteur_anc').value)
       if(consomation == Infinity){
         return
-      }else if(consomation == NaN){
-        this.PointageFormAddSecond.get('consomation').setValue(0)
+      }else if(value-this.PointageFormAdd.get('compteur_anc').value == 0){
+        this.PointageFormAdd.get('consomation').setValue(0)
         return
       }else if(consomation < 0){
-        this.PointageFormAddSecond.get('consomation').setValue(0)
+        this.PointageFormAdd.get('consomation').setValue(0)
       }else{
-        this.PointageFormAddSecond.get('consomation').setValue(Math.round(consomation))
+        this.PointageFormAdd.get('consomation').setValue(Math.round(consomation))
       }
     })
 
-    this.PointageFormAddFirst.get('etat_f').valueChanges.subscribe(value => {
+    this.PointageFormAdd.get('heure_m').valueChanges.subscribe(value => {
+      let heure_arr = this.PointageFormAdd.get('heure_ar').value
+      let heure_pan = this.PointageFormAdd.get('heure_p').value
+      if(heure_arr+heure_pan+value > 10){
+        this.PointageFormAdd.get('heure_m').setValue(10-(heure_arr+heure_pan))
+      }
+    })
+
+    this.PointageFormAdd.get('heure_ar').valueChanges.subscribe(value => {
+      let heure_ma = this.PointageFormAdd.get('heure_m').value
+      let heure_pan = this.PointageFormAdd.get('heure_p').value
+      if(heure_ma+heure_pan+value > 10){
+        this.PointageFormAdd.get('heure_ar').setValue(10-(heure_ma+heure_pan))
+      }
+    })
+
+    this.PointageFormAdd.get('heure_p').valueChanges.subscribe(value => {
+      let heure_ma = this.PointageFormAdd.get('heure_m').value
+      let heure_arr = this.PointageFormAdd.get('heure_ar').value
+      if(heure_ma+this.heure_ar+value > 10){
+        this.PointageFormAdd.get('heure_p').setValue(10-(heure_ma+heure_arr))
+      }
+    })
+
+    this.PointageFormAdd.get('compteur_nvx').valueChanges.subscribe(value => {
+      var consomation = (this.PointageFormAdd.get('gasoil').value*100)/(value-this.PointageFormAdd.get('compteur_anc').value)
+      if(consomation == Infinity){
+        return
+      }else if(value-this.PointageFormAdd.get('compteur_anc').value == 0){
+        this.PointageFormAdd.get('consomation').setValue(0)
+        return
+      }else if(consomation < 0){
+        this.PointageFormAdd.get('consomation').setValue(0)
+      }else{
+        this.PointageFormAdd.get('consomation').setValue(Math.round(consomation))
+      }
+    })
+
+    this.PointageFormAdd.get('etat_f').valueChanges.subscribe(value => {
       switch (value) {
         case 'MARCHE':
-          this.PointageFormAddFirst.get('heure_m').setValue(10)
-          this.PointageFormAddFirst.get('heure_p').setValue(0)
-          this.PointageFormAddFirst.get('heure_ar').setValue(0)
-          this.PointageFormAddFirst.get('heure_m').enable()
-          this.PointageFormAddFirst.get('heure_p').setValue(0)
-          this.PointageFormAddFirst.get('heure_p').enable()
-          this.PointageFormAddFirst.get('heure_ar').setValue(0)
-          this.PointageFormAddFirst.get('heure_ar').enable()
+          this.PointageFormAdd.get('heure_p').setValue(0)
+          this.PointageFormAdd.get('heure_ar').setValue(0)
+          this.PointageFormAdd.get('heure_m').setValue(10)
           break;
         case 'ARRET':
-          this.PointageFormAddFirst.get('heure_m').setValue(0)
-          this.PointageFormAddFirst.get('heure_p').setValue(0)
-          this.PointageFormAddFirst.get('heure_ar').setValue(10)
-          this.PointageFormAddFirst.get('heure_m').enable()
-          this.PointageFormAddFirst.get('heure_p').enable()
-          this.PointageFormAddFirst.get('heure_ar').enable()
+          this.PointageFormAdd.get('heure_m').setValue(0)
+          this.PointageFormAdd.get('heure_p').setValue(0)
+          this.PointageFormAdd.get('heure_ar').setValue(10)
           break;
         case 'MAD':
-          this.PointageFormAddFirst.get('heure_m').setValue(0)
-          this.PointageFormAddFirst.get('heure_m').disable()
-          this.PointageFormAddFirst.get('heure_p').setValue(0)
-          this.PointageFormAddFirst.get('heure_p').disable()
-          this.PointageFormAddFirst.get('heure_ar').setValue(0)
-          this.PointageFormAddFirst.get('heure_ar').disable()
+          this.PointageFormAdd.get('heure_m').setValue(0)
+          this.PointageFormAdd.get('heure_p').setValue(0)
+          this.PointageFormAdd.get('heure_ar').setValue(0)
           break;
         case 'PANNE':
-          this.PointageFormAddFirst.get('heure_m').setValue(0)
-          this.PointageFormAddFirst.get('heure_p').setValue(10)
-          this.PointageFormAddFirst.get('heure_ar').setValue(0)
-          this.PointageFormAddFirst.get('heure_m').enable()
-          this.PointageFormAddFirst.get('heure_p').enable()
-          this.PointageFormAddFirst.get('heure_ar').enable()
+          this.PointageFormAdd.get('heure_m').setValue(0)
+          this.PointageFormAdd.get('heure_ar').setValue(0)
+          this.PointageFormAdd.get('heure_p').setValue(10)
           break;
         case 'EN ATTENTE':
-          this.PointageFormAddFirst.get('heure_m').setValue(0)
-          this.PointageFormAddFirst.get('heure_p').setValue(0)
-          this.PointageFormAddFirst.get('heure_ar').setValue(0)
-          this.PointageFormAddFirst.get('heure_m').disable()
-          this.PointageFormAddFirst.get('heure_p').disable()
-          this.PointageFormAddFirst.get('heure_ar').disable()
+          this.PointageFormAdd.get('heure_m').setValue(0)
+          this.PointageFormAdd.get('heure_p').setValue(0)
+          this.PointageFormAdd.get('heure_ar').setValue(0)
           break;
         default:
           break;
@@ -182,6 +207,8 @@ export class PointageAddComponent implements OnInit {
         map(value => this._filter_chantier(value))
       );
   }
+
+
   private _filter_chantier(value: string): string[] {
     if(value === null){
       return
@@ -190,46 +217,15 @@ export class PointageAddComponent implements OnInit {
 
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
-
-  /* Get errors */
-  public handleError = (controlName: string, errorName: string) => {
-    return this.PointageFormAddFirst.controls[controlName].hasError(errorName);
-  }
-
-/** */
+  /** */
   getControls(event$){
-    this.myControl.setValue(event$.option.value)
-    this.PointageFormAddFirst.controls['id_chantier'].setValue(event$.option.id)
+    this.PointageFormAdd.controls['id_chantier'].setValue(eval(event$.option.id))
+    this.PointageFormAdd.controls['myControl'].setValue(event$.option.value)
   }
 
-    /* Reset form */
-    closeForm() {
-      this.dialogRef.close();
-    }
-
-    doSomething(event$){
-      console.log(event$)
-    }
-
-    panneFunction(){
-      this.PointageFormAddFirst.get('etat_f').setValue('PANNE')
-      this.PointageFormAddFirst.get('heure_ar').setValue(0)
-      this.PointageFormAddFirst.get('heure_p').setValue(10)
-      this.PointageFormAddFirst.get('heure_m').setValue(0)
-    }
-
-    marcheFunction(){
-      this.PointageFormAddFirst.get('etat_f').setValue('MARCHE')
-      this.PointageFormAddFirst.get('heure_ar').setValue(0)
-      this.PointageFormAddFirst.get('heure_p').setValue(0)
-      this.PointageFormAddFirst.get('heure_m').setValue(10)
-    }
-
-    arretFunction(){
-      this.PointageFormAddFirst.get('etat_f').setValue('ARRET')
-      this.PointageFormAddFirst.get('heure_ar').setValue(10)
-      this.PointageFormAddFirst.get('heure_p').setValue(0)
-      this.PointageFormAddFirst.get('heure_m').setValue(0)
+    /* Get errors */
+    public handleError = (controlName: string, errorName: string) => {
+      return this.PointageFormAdd.controls[controlName].hasError(errorName);
     }
 
 }
