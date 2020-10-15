353ad65017f193c42774dd59845fa26412615fb4
AHINCH ADIL <ihnichadil@gmail.com>
2020-10-15 23:22:51 +0100

    Update pointage-engin.component.ts

--- src/app/pointage-list/pointage-engin/pointage-engin.component.ts
+++ src/app/pointage-list/pointage-engin/pointage-engin.component.ts
@@ -26,7 +26,7 @@ export class PointageEnginComponent implements OnInit {
       this.engin = this.router.getCurrentNavigation().extras.state.engins
     }
 
-  displayedColumns: string[] = ['date_pointage', 'chantier', 'type_p','heure_m','heure_ar','heure_p','gasoil','compteur','compteur_p','consomation','etat_compteur','displayName'];
+  displayedColumns: string[] = ['date_pointage', 'chantier','mobile', 'type_p','heure_m','heure_ar','heure_p','gasoil','compteur','compteur_p','consomation','etat_compteur','chauffeur','displayName'];
   dataSource : MatTableDataSource<Pointage>;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
