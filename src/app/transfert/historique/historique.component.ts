f0e0a056ccc595e702ffd04737776403cbab09c0
AHINCH ADIL <ihnichadil@gmail.com>
2020-10-16 15:25:28 +0100

    Update historique.component.ts

--- src/app/transfert/historique/historique.component.ts
+++ src/app/transfert/historique/historique.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort, MatDialog } from '@angular/material';
+import { MatPaginator} from '@angular/material/paginator';
+import { TransfertService } from './../../services/transfert.service';
+import { Transfert } from './../../models/engin.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-historique',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoriqueComponent implements OnInit {
 
-  constructor() { }
+  collectionPermAdd: boolean
+  collectionPermUpdate: boolean
+  collectionPermDelete: boolean
+  collectionMenuToggel:boolean
+  displayedColumns: string[] = ['numero','code','designation','accessoire','destination','demandeur','provenance','accord','date'];
+  dataSource : MatTableDataSource<Transfert>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static : true}) sort: MatSort;
+  constructor(
+    private transfertService:TransfertService,
+    public dialog: MatDialog,
+    public router:Router
+    ) { }
 
-  ngOnInit() {
+  ngOnInit():void {
+    this.transfertService.getTransfertHistorique().subscribe(
+      data => {
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.paginator._intl.itemsPerPageLabel = 'Affichage par page.';
+        this.paginator._intl.firstPageLabel = 'Page Premier';
+        this.paginator._intl.nextPageLabel = 'Page Suivant';
+        this.paginator._intl.previousPageLabel = 'Page Précédante';
+        this.paginator._intl.lastPageLabel = 'Dérnier Page';
+      }
+    )
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+      this.dataSource.sort = this.sort;
+    }
   }
 
 }
