ffcc7b18765fe538512af0f0d7c081eb7b54f300
AHINCH ADIL <ihnichadil@gmail.com>
2020-10-16 15:25:31 +0100

    Update transfert.component.ts

--- src/app/transfert/transfert.component.ts
+++ src/app/transfert/transfert.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransfertComponent implements OnInit {
 
-  constructor() { }
+  collectionPermAdd: boolean
+  constructor(
+  ) {
+  }
 
   ngOnInit() {
   }
