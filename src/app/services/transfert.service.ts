bd86857f93705a46382ab4cb2791242dd3b2b2e2
AHINCH ADIL <ihnichadil@gmail.com>
2020-10-22 17:36:10 +0100

    Update transfert.service.ts

--- src/app/services/transfert.service.ts
+++ src/app/services/transfert.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { map } from 'rxjs/operators';
-import { Transfert } from '../models/engin.model';
+import { Notification, Transfert } from '../models/engin.model';
+import { AuthService } from './auth.service';
+import { EnginService } from './engin.service';
 declare var require: any;
 const firebase = require('firebase')
 
@@ -10,17 +12,23 @@ const firebase = require('firebase')
 })
 export class TransfertService {
 
-  constructor(private afs: AngularFirestore) {}
+  constructor(private afs: AngularFirestore,private enginService : EnginService,private auth : AuthService) {}
   getTransfertEnCours(){
-    return this.afs.collection<Transfert>('notification',ref=>{
+    return this.afs.collection<Notification>('notification',ref=>{
       let query : firebase.firestore.Query = ref
       query = query.where('etat','<',6)
       return query
     }).snapshotChanges().pipe(map(action=>{
       return action.map(a=>{
         const id = a.payload.doc.id
-        const data = a.payload.doc.data() as Transfert
-        return {id, ...data}
+        const data = a.payload.doc.data() as Notification
+        const engin_id = a.payload.doc.data().engin
+        const user_uid = a.payload.doc.data().uid
+        const user_uid_validation = a.payload.doc.data().validation
+        let engins = this.enginService.GetEnginListByIdArray([engin_id])
+        let users = this.auth.user$(user_uid)
+        let users_validation = this.auth.user$(user_uid_validation)
+        return {id,engins,users,users_validation, ...data}
       })
     }))
   }
