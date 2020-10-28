bba3c71712589bd1c102609c4c540115d851088c
AHINCH ADIL <ihnichadil@gmail.com>
2020-10-28 17:51:59 +0100

    Update region.service.ts

--- src/app/services/region.service.ts
+++ src/app/services/region.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Chantier, Region } from '../models/engin.model';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { map, mergeMap, switchMap } from 'rxjs/operators';
+import { map, } from 'rxjs/operators';
 import { firestore } from 'firebase';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable, combineLatest } from 'rxjs';
-import { of } from 'rxjs';
+import { ChantierService } from './chantier.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegionService {
-	constructor(private afs: AngularFirestore,private firebaseAuth: AngularFireAuth) {}
+	constructor(private afs: AngularFirestore,private firebaseAuth: AngularFireAuth,private chantierService:ChantierService) {}
 	/* Créer une nouvelle Region */
 	AddRegion(region: Region){
 		return this.afs.collection('region').doc(region.id.toString()).set({
@@ -28,28 +27,17 @@ export class RegionService {
 
 	/* Retourn une liste des regions */
 	GetRegionList() {
-		/* return this.afs.collection<Region[]>('region',ref=> ref.orderBy('createdAt','asc')).snapshotChanges().pipe(
+		return this.afs.collection<Region>('region',ref=> ref.orderBy('createdAt','asc')).snapshotChanges().pipe(
 			map(actions => {
 			return actions.map(a => {
-				const data = a.payload.doc.data() as Region[];
+				const data = a.payload.doc.data() as Region;
         const id = a.payload.doc.id;
-				return {id, ...data}
+        const name = a.payload.doc.data().name
+        let sites = this.chantierService.getChantierByRegion(name)
+				return {id, ...data,sites}
 			});
 			})
-    ); */
-
-    return this.afs.collection<Region>('region').snapshotChanges().pipe(
-      switchMap((regions)=>{
-        const region = regions.map(r=>{
-          return this.afs.collection('chantier',ref=> ref.where('region','==',r.payload.doc.data().name)).snapshotChanges()
-          .pipe(
-            map(sites=>Object.assign(r.payload.doc.data(),{sites}))
-          )
-        })
-        return combineLatest(...region)
-      }
-      )
-    )
+    );
   }
 
 
@@ -67,7 +55,7 @@ export class RegionService {
 			updatedBy: this.firebaseAuth.auth.currentUser.uid,
 			updatedAt: firestore.FieldValue.serverTimestamp(),
 			name: region.name.toUpperCase(),
-			compte: region.compte
+			compte: region.compte.toUpperCase()
 		})
 	}
 }
