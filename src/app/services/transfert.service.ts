3403e372ef36b402551c8a3a77aa3e8179af85a3
AHINCH ADIL <ihnichadil@gmail.com>
2020-11-04 10:50:55 +0100

    Update transfert.service.ts

--- src/app/services/transfert.service.ts
+++ src/app/services/transfert.service.ts
@@ -33,15 +33,21 @@ export class TransfertService {
     }))
   }
   getTransfertHistorique(){
-    return this.afs.collection<Transfert>('notification',ref=>{
+    return this.afs.collection<Notification>('notification',ref=>{
       let query : firebase.firestore.Query = ref
       query = query.where('etat','>=',6)
       return query
     }).snapshotChanges().pipe(map(action=>{
       return action.map(a=>{
         const id = a.payload.doc.id
-        const data = a.payload.doc.data() as Transfert
-        return {id, ...data}
+        const data = a.payload.doc.data() as Notification
+        const engin_id = a.payload.doc.data().engin
+        const user_uid = a.payload.doc.data().uid
+        const user_uid_validation = a.payload.doc.data().validation
+        let engins = this.enginService.GetEnginListByIdArray([engin_id])
+        let users = this.auth.user$(user_uid)
+        let users_validation = this.auth.user$(user_uid_validation)
+        return {id,engins,users,users_validation, ...data}
       })
     }))
   }
