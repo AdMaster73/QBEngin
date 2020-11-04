e62f2d3a955d48e82fd83a5bebb0ac47e46c84d2
AHINCH ADIL <ihnichadil@gmail.com>
2020-11-04 10:50:42 +0100

    Update categorie.service.ts

--- src/app/services/categorie.service.ts
+++ src/app/services/categorie.service.ts
@@ -1,59 +1,73 @@
 import { Injectable } from '@angular/core';
-import { Categorie } from '../models/engin.model';
+import { Categorie, ICategorieResponse } from '../models/engin.model';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { map } from 'rxjs/operators';
 import { firestore } from 'firebase';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategorieService {
-		
+
 	constructor(private afs: AngularFirestore,private firebaseAuth: AngularFireAuth) {}
 	/* Créer une nouvelle Categorie */
-	AddCategorie(categorie: Categorie){		
+	AddCategorie(categorie: Categorie){
 		return this.afs.collection('categorie').doc(categorie.id.toString()).set({
 			createdBy: this.firebaseAuth.auth.currentUser.uid,
-			createdAt: firestore.FieldValue.serverTimestamp(),  			     
-			name: categorie.name		
+			createdAt: firestore.FieldValue.serverTimestamp(),
+			name: categorie.name
 		})
-	}
-	
+  }
+
+  GetCategorieListSearch():Observable<ICategorieResponse> {
+    return new Observable(subscriber => {
+      this.GetCategorieList().subscribe(categories=>{
+        subscriber.next({
+          total: categories.length,
+          results: categories
+        });
+      })
+    });
+}
+
 	/* Supprimer la categorie */
 	async DeleteCategorie(id) {
 		this.afs.doc('categorie/'+id).delete()
 	}
 
 	/* Retourn une liste des categories */
-	GetCategorieList() {		
-		return this.afs.collection<Categorie>('categorie',ref=> ref.orderBy('createdAt','asc')).snapshotChanges().pipe(
+	GetCategorieList() {
+    return this.afs.collection<Categorie>('categorie',ref=> ref.orderBy('createdAt','asc'))
+    .snapshotChanges()
+    .pipe(
 			map(actions => {
 			return actions.map(a => {
 				const data = a.payload.doc.data() as Categorie;
-				const id = a.payload.doc.id;
+        const id = a.payload.doc.id;
 				return { id, ...data };
 			});
 			})
-		);	
+		);
 	}
-	
+
 	//Retourne le ID du dernier enregistrement
-	GetCategorieLastRecord(){		
+	GetCategorieLastRecord(){
 	  return this.afs.collection('categorie', ref => ref
 		.limit(1)
 		.orderBy('createdAt','desc')
-	  )				
+	  )
 	}
-	
+
 	/* Modifier la categorie */
-	UpdateCategorie(categorie) { 
+	UpdateCategorie(categorie) {
 		this.afs.doc('categorie/'+categorie.id).update({
 			updatedBy: this.firebaseAuth.auth.currentUser.uid,
-			updatedAt: firestore.FieldValue.serverTimestamp(),	
+			updatedAt: firestore.FieldValue.serverTimestamp(),
 			name: categorie.name.toUpperCase(),
-			compte: categorie.compte			
-		})														
-	} 
+			compte: categorie.compte
+		})
+	}
 
 }
