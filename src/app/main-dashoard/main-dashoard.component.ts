e8338b37e64ca28d704d85fb39928dbb1e5b9d55
AHINCH ADIL <ihnichadil@gmail.com>
2021-04-17 19:13:36 +0000

    Update main-dashoard.component.ts

--- src/app/main-dashoard/main-dashoard.component.ts
+++ src/app/main-dashoard/main-dashoard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Chantier } from '../models/engin.model';
 import { ChantierService } from '../services/chantier.service';
+import {NavigationExtras, Router} from '@angular/router';
 
 
 @Component({
@@ -14,9 +15,17 @@ export class MainDashoardComponent implements OnInit{
   chantier$ : Observable<Chantier[]>;
 
   constructor(
+    private router : Router,
     private chantierService : ChantierService
     ) {}
   ngOnInit() {
     this.chantier$ = this.chantierService.getChantierByUser()
+  }
+
+  forwordSites(chantier : Chantier){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {chantier}
+    }
+    this.router.navigate(['/sidenav/site-details'],{state:{chantier:navigationExtras}})
   }
 }
