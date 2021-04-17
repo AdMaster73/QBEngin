a4fbeb1d4adbe2ea7c7f8a0db8d1a20cead65717
AHINCH ADIL <ihnichadil@gmail.com>
2021-04-17 19:33:32 +0000

    Update pointage-list.component.ts

--- src/app/pointage-list/pointage-list.component.ts
+++ src/app/pointage-list/pointage-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatTableDataSource, MatSort, MatDialog, MatDialogConfig} from '@angular/material';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import { EnginService } from '../services/engin.service';
 import { Chantier, Engin, Pointage, Region } from './../models/engin.model';
