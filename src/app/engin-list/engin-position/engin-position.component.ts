1788248d7c83a50faa66d16aaa0f7582ad6d5db0
AHINCH ADIL <ihnichadil@gmail.com>
2021-04-17 19:32:19 +0000

    Update engin-position.component.ts

--- src/app/engin-list/engin-position/engin-position.component.ts
+++ src/app/engin-list/engin-position/engin-position.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { EnginService } from 'src/app/services/engin.service';
 import { EnginListComponent } from '../engin-list.component';
 
