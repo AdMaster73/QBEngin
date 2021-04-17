fab05cc04e1c837f71c905e82468c5dbb3980c83
AHINCH ADIL <ihnichadil@gmail.com>
2021-04-17 19:10:37 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,46 @@
 import { Component} from '@angular/core';
 import 'rxjs/add/observable/interval';
-import { Subscription } from 'rxjs';
-import { AuthService } from './services/auth.service'
-
+import { Subscription, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
-  selector: "app-root",  
+  selector: "app-root",
   templateUrl:'./app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 	  secondes: number;
 	  counterSubscription: Subscription;
-	  isAuth : number; 
-	  
-	  constructor(private authService : AuthService) {}	 		
+	  isAuth : number;
+    //private _jsonURL = 'assets/mouvements.json';
+
+	  constructor(private http: HttpClient,private afs: AngularFirestore) {
+      /* this.getJSON().subscribe(data => {
+        data.forEach(mouvements => {
+          var date_mvt_string = new Date(mouvements.date_mvt).getFullYear()
+          if(date_mvt_string.toString() == '2021'){
+                this.afs.collection('charges').doc(date_mvt_string.toString())
+              .collection('mouvements')
+              .doc(mouvements.id)
+              .set({
+                code_materiel: mouvements.code_materiel,
+                libelle_materiel:mouvements.libelle_materiel,
+                num_bon:mouvements.num_bon,
+                code_artice:mouvements.code_artice,
+                nom_produit:mouvements.nom_produit,
+                num_mvt : mouvements.num_mvt,
+                qte:eval(mouvements.qte),
+                cout_mvt:eval(mouvements.cout_mvt),
+                date_mvt:new Date(mouvements.date_mvt)
+              })
+          }else{
+              return
+          }
+        });
+       }); */
+    }
+    /* public getJSON(): Observable<any> {
+      return this.http.get(this._jsonURL);
+    } */
 }
