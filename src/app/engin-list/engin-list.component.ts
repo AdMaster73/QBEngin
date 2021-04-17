dcd22893e54182f1fcd2cc1545bf87a07477bb99
AHINCH ADIL <ihnichadil@gmail.com>
2021-04-17 19:32:14 +0000

    Update engin-list.component.ts

--- src/app/engin-list/engin-list.component.ts
+++ src/app/engin-list/engin-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import {MatTableDataSource, MatSort, MatDialog} from '@angular/material';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import { EnginService } from '../services/engin.service';
 import { CollectionsService } from '../services/collections.service'
